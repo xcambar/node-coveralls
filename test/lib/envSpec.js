27177246a39807d5dfa4e523d62c7dc4f6f0764f
Xavier Cambar <xcambar@gmail.com>
2013-06-19 00:49:57 +0200

    Fixes the signatures and specs

--- test/lib/envSpec.js
+++ test/lib/envSpec.js
@@ -1,18 +1,17 @@
 "use strict";
 
 var should = require('should');
+var env = {};
 require('log-driver')({level : false});
-var module = require('../../lib/env');
+var module = require('../../lib/env')(env);
 
 describe ('Environment requirements', function () {
   it('should pass with a repo token', function () {
-    module.processEnv = {
-      COVERALLS_REPO_TOKEN: 'OK'
-    };
+    env.COVERALLS_REPO_TOKEN = 'OK';
     module.buildEnv.bind(undefined).should.not.throw();
+    delete env.COVERALLS_REPO_TOKEN;
   });
   it('should fail with neither a repo token nor a sevice+job info', function () {
-    module.processEnv = {};
     module.buildEnv.bind(undefined).should.throw();
   });
 });
